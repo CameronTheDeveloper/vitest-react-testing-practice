b924ab3e26a4769719ed3fc7d75a6a7145d047a7
Cameron Posey <cameronposey@outlook.com>
2024-01-11 22:26:53 -0800

    Add name option in header test

--- tests/App.test.jsx
+++ tests/App.test.jsx
@@ -6,7 +6,7 @@ import { expect } from 'vitest';
 describe('App', () => {
   it('renders correct heading', () => {
     render(<App></App>);
-    expect(screen.getByRole('heading').textContent).toMatch(/First Test/i)
+    expect(screen.getByRole('heading', {name: 'First Test'}).textContent).toMatch(/First Test/i)
 
   });
 });
